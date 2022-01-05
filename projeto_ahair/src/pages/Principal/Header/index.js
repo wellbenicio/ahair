15e39859a1fce56602e1c0d0eba7058e225f09c7
wellbenicio <wellynton.benicio@gmail.com>
2022-01-05 20:57:28 -0300

    Estruturação pastas e inicio da estruturação html

---
+++ projeto_ahair/src/pages/Principal/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <div>
+                <img></img>
+            </div>
+            <div>
+                <ul>
+                    <li>Serviços</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+            <div>
+                <button>Ver horários</button>
+            </div>
+        </div>
+    )
+}
+
+export default index
---
+++ projeto_ahair/src/pages/Principal/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './Header/index'
+
+const index = () => {
+    return (
+        <div>
+            <Header/>
+            <div>
+                <img></img>
+            </div>
+            <div>
+                <div>
+                    <h1>Beleza em cada dia de sua vida</h1>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                </div>
+            </div>
+
+            <div>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default index
