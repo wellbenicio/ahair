10d1f83ce282a8a4d234d5aeb751c6500a8b8c29
wellbenicio <wellynton.benicio@gmail.com>
2022-01-05 21:30:00 -0300

    Estruturação Header da página principal

--- projeto_ahair/src/pages/Principal/Header/index.js
+++ projeto_ahair/src/pages/Principal/Header/index.js
@@ -1,20 +1,37 @@
 import React from 'react'
-
+import Navbar from '../../../components/Navbar/index'
 const index = () => {
     return (
         <div>
+            <Navbar />
             <div>
                 <img></img>
             </div>
             <div>
-                <ul>
-                    <li>Serviços</li>
-                    <li>Sobre</li>
-                    <li>Contato</li>
-                </ul>
+                <div>
+                    <h1>Beleza em cada dia de sua vida</h1>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                </div>
+            </div>
+
+            <div>
+                <button>Agendar horário</button>
             </div>
             <div>
-                <button>Ver horários</button>
+                <Profissionais />
+                <Profissionais />
+                <Profissionais />
+                <Profissionais />
+                <Profissionais />
+                <Profissionais />
+                <Profissionais />
+                <Profissionais />
+                <div>
+                    <p>Ver todos</p>
+                    <img src={} alt={}/>
+                </div>
             </div>
         </div>
     )
--- projeto_ahair/src/pages/Principal/index.js
+++ projeto_ahair/src/pages/Principal/index.js
@@ -1,25 +1,12 @@
 import React from 'react'
 import Header from './Header/index'
+import Profissionais from './Profissionais/index'
 
 const index = () => {
     return (
         <div>
             <Header/>
-            <div>
-                <img></img>
-            </div>
-            <div>
-                <div>
-                    <h1>Beleza em cada dia de sua vida</h1>
-                </div>
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
-                </div>
-            </div>
-
-            <div>
-                <button></button>
-            </div>
+            <Body />
         </div>
     )
 }
